bf52f554dc2176d5490b22fb547855db690aa40c
Nguyễn Hưng Thịnh <nhtnokia@gmail.com>
2021-06-11 13:18:34 +0700

    Check step 2 for logged or not

--- src/pages/SigninPage/index.js
+++ src/pages/SigninPage/index.js
@@ -14,7 +14,7 @@ const SignIn = () => {
     const [data, setData] = useState([]);
     const [msgError, setMsgError] = useState("");
     const [isLogged, setIsLogged] = useState(false);
-    const [localLoginStatus, setLocalLoginStatus] = useLocalStorage("is_login", false);
+    const [localLoginStatus, setLocalLoginStatus] = useLocalStorage("is_logged", false);
 
 
     const validateInfo = () => {
@@ -72,29 +72,31 @@ const SignIn = () => {
         }
     }
 
-    // useEffect(() => {
-    //     console.log("RUNNED");
-    //     const cancelTokenSource = axios.CancelToken.source();
+    useEffect(() => {
+        console.log("RUNNED");
+        const cancelTokenSource = axios.CancelToken.source();
 
-    //     console.log("hi");
-    //     console.log(localLoginStatus);
-
-    //     axios.post(API.CHECK_VALID_TOKEN, null,
-    //         {
-    //             withCredentials: true,
-    //             cancelToken: cancelTokenSource.token
-    //         })
-    //         .then(async function (response) {
-    //             if (response.status == 200) {
-    //                 console.log(" HAHAHAHAHA SUCCESS | NEED NAV LINK TO HOME");
-    //                 setIsLogged(true);
-    //             }
-    //         })
-    //         .catch(function (error) {
-    //             console.log("Still do not signed in");
-    //         });
-    //     return () => cancelTokenSource.cancel();
-    // }, [isLogged]);
+        console.log("hi");
+        console.log(localLoginStatus);
+        if (!localLoginStatus) {
+            axios.post(API.CHECK_VALID_TOKEN, null,
+                {
+                    withCredentials: true,
+                    cancelToken: cancelTokenSource.token
+                })
+                .then(async function (response) {
+                    if (response.status == 200) {
+                        console.log(" HAHAHAHAHA SUCCESS | NEED NAV LINK TO HOME");
+                        setIsLogged(true);
+                        setLocalLoginStatus(true);
+                    }
+                })
+                .catch(function (error) {
+                    console.log("Still do not signed in");
+                });
+        }
+        return () => cancelTokenSource.cancel();
+    }, [isLogged]);
 
     if (localLoginStatus) return <Redirect to="/" />
     return (
